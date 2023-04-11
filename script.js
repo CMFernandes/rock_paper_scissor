c11e961df1316b8813e0dc54bb32c75c14d8bcba
cmfernandes <cristianafmfernandes@gmail.com>
2023-04-11 10:05:00 +0100

    script.js five rounds game done

--- script.js
+++ script.js
@@ -14,23 +14,84 @@ function getComputerChoice() {
     return possibleHands[indexOfPossibleHands];
 }
 
-//fuction that plays a single round
+let playerScore = 0;
+let computerScore = 0;
+
+function game(){
+    let playerSelection;
+    let computerSelection;
+    let result;
+
+    
+
+    for (let i = 1 ; i <= 5; i++){
+        playerSelection = prompt().toLowerCase();
+
+        while(!validateHand(playerSelection)) {
+            alert("Not a valid hand!");
+            playerSelection = prompt().toLowerCase();
+        }
+
+        computerSelection = getComputerChoice();
+
+        console.log(`Your hand: ${playerSelection}`);
+        console.log(`Computer hand: ${computerSelection}`);
+        
+
+        result = playRound(playerSelection, computerSelection);
+
+        evaluateRound(result);  
+    }
+    console.log(`Your score: ${playerScore}.`)
+    console.log(`Computer ${computerScore}.`)
+    return evaluateResult(playerScore, computerScore);
+
+}
+
+function validateHand(playerSelection){
+    if (playerSelection == "rock" || 
+        playerSelection == "paper" || 
+        playerSelection == "scissor" ){
+        return true
+    }else {
+        return false
+    }
+}
+
+function evaluateResult(playerScore, computerScore){
+    if(playerScore > computerScore) {
+        return "You Win!";
+    } else if(playerScore < computerScore){
+        return "You Lost!";
+    } else {
+        return "It's a tie!";
+    }
+}
+
+function evaluateRound(result){
+    if(result == 0) {
+        console.log("It's a tie!");
+    } else if(result == 1) {
+        console.log("Round win!");
+        playerScore++;        
+    } else {
+        console.log("Round lost!");
+        computerScore++;
+    }
+
+}
 function playRound(playerSelection,computerSelection){
-    console.log(`You choose ${playerSelection} and the computer choose ${computerSelection}.`)
     if (playerSelection == computerSelection){
-        return `It's a tie!`
+        return 0;
     }else if ((playerSelection == "paper" && computerSelection == "rock")  ||
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper")) {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        return 1;
     }else{
-        return `You Loose! ${computerSelection} beats ${playerSelection}!`
+        return -1;
     }
 
 
 }
 
-const playerSelection = prompt().toLowerCase();
-//const containing computer random selection
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
